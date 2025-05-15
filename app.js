0a51a6d97b5e05fb45a45f0223764b2b68772c17
stittpaul2001 <stittpaul2001@gmail.com>
2025-05-15 15:20:08 -0600

    worked on the layout more and got more functional clicks happening

--- app.js
+++ app.js
@@ -39,15 +39,51 @@ let automaticUpgrades = [
 function mine() {
   let totalBonus = 1
   clickUpgrades.forEach(upgrade => {
-    totalBonus += upgrade.quantity * upgrade.bonus
+    totalBonus += upgrade.bonus * upgrade.quantity
   })
   cheese += totalBonus
   updateCheeseDisplay()
   console.log(`you have mined ${cheese} cheese`)
 }
 
+function buyPickaxe() {
+  const pickaxe = clickUpgrades.find(upgrade => upgrade.name === 'pickaxe')
+  if (cheese >= pickaxe.price) {
+    cheese -= pickaxe.price
+    pickaxe.price++
+    pickaxe.quantity++
+    console.log('You purchased a pickaxe')
+    updateCheeseDisplay()
+  }
+  updatePickaxe()
+}
+
+function buyDrill() {
+  const drill = clickUpgrades.find(Upgrade => Upgrade.name === 'drill')
+  if (cheese >= drill.price) {
+    cheese -= drill.price
+    drill.price++
+    drill.quantity++
+    console.log('You purchased a drill')
+    updateCheeseDisplay()
+  }
+  updateDrill()
+}
+
 // Section-Draw Section
 function updateCheeseDisplay() {
   document.getElementById('cheese-amount').innerText = `Cheese Bank:🧀 ${cheese}`
+}
+
+function updatePickaxe() {
+  const pickaxe = clickUpgrades.find(upgrade => upgrade.name === 'pickaxe')
+  let pickaxeElem = document.getElementById('pickaxe-quantity')
+  pickaxeElem.innerText = `${pickaxe.quantity}`
+}
+
+function updateDrill() {
+  const drill = clickUpgrades.find(upgrade => upgrade.name === 'drill')
+  let drillElem = document.getElementById('drill-quantity')
+  drillElem.innerText = `${drill.quantity}`
 }
 // setInterval(collectAutoUpgrades, 3000);
